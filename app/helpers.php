868190dbfe2f7d1af5cbdf954b0cec3daa7f70ca
yurii <0969966@gmail.com>
2022-09-13 23:21:52 +0300

    feat: add helpers.php (for autoload)

---
+++ app/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('myCustomFunction')) {
+    /**
+     * Description for my function
+     *
+     * @param string $myParameter
+     * @return void
+     */
+    function myCustomFunction(string $myParameter): void
+    {
+        //do something
+    }
+}
